ad51168b1bcdc91591a96a4c4dd91d711a5fda1b
Jason Carman <jasonmcarman@gmail.com>
2023-04-29 22:11:57 -0400

    Add files via upload
    Added hello.bash and hello.py samples from week 2.

---
+++ hello.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author: Jason Carman
+# Date: January 10, 2023
+# Purpose: Print hello, list the contents of /, then print goodbye
+# Usage: ./hello.bash
+
+# Print hello on the screen
+echo "Hello"
+
+# List the contents of /
+ls /
+
+# Print good bye on the screen
+echo "Good Bye
